24bdaf3be1d28c2d242659d14df4194a1357398d
Andres Kwan <andreskwan@gmail.com>
2021-06-07 18:06:47 -0500

    [Singletons] - Extensions are allowed on final classes!!!

--- 01-Singletons.playground/Contents.swift
+++ 01-Singletons.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
+final class AppState {
+    static let sharedInstance = AppState()
+    
+    private init() { }
+}
+
+let appState = AppState.sharedInstance
+//I can't instantiate a singleton
+//let appState2 = AppState()
+
+//Extensions are allowed on final classes!!!
+extension AppState {
+    func addition(_ a: Int, _ b: Int) -> Int {
+        a + b
+    }
+}
+
+appState.addition(3, 7)
+
 
