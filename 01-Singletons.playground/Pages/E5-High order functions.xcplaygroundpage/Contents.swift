99c333164cb43d304ead890201d331548f682d13
Andres Kwan <andreskwan@gmail.com>
2021-09-09 16:29:18 -0500

    [High Order Functions] - filter with map and compactMap

---
+++ 01-Singletons.playground/Pages/E5-High order functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+//: [Previous](@previous)
+
+import Foundation
+let mandy = "Mandi"
+
+let characteres = mandy.map{ $0 }
+characteres
+let variable = characteres.reduce(""){ $0 + String($1) }
+variable
+
+var vowels = mandy.filter{"aeiou".contains($0)}
+
+let vowels2 = "Kira".map{ char -> String? in
+    if("aeiou".contains(char)) {
+        return String(char)
+    }
+    return nil
+}.compactMap{ $0 }
+
+vowels2
+
+let eages = [(name: "Kira",6),(name: "Mandy",2),(name: "Miel",1)].map{ $0.name }
+eages
+
+(0...10).map{ print($0)}
+
+//: [Next](@next)
