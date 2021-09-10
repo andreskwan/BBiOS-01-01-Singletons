a6dff2e547b3065a9659e3dcffc67e6cec9803cf
Andres Kwan <andreskwan@gmail.com>
2021-09-10 03:54:23 -0500

    [E6] - Enums - associated values (storing data) & pattern matching

---
+++ 01-Singletons.playground/Pages/E6-ValidData.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+//: [Previous](@previous)
+
+import Foundation
+
+struct FeedItem: Equatable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
+
+enum Barcode {
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
+}
+
+var productBarcode = Barcode.upc(8, 85909, 51226, 3)
+
+switch productBarcode {
+case .upc(let numberSystem, let manufacturer, let product, let check):
+    print("UPC: \(numberSystem), \(manufacturer), \(product), \(check).")
+case .qrCode(let productCode):
+    print("QR code: \(productCode).")
+}
+
+//: [Next](@next)
